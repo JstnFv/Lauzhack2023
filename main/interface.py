0b1a6a66e0bd7a49a70559b24de03c96bf11fdee
nikhen-s <nikhen.sanjaya2003@gmail.com>
2023-12-03 03:20:09 +0100

    Compatible with DJango

--- main/interface.py
+++ main/interface.py
@@ -8,7 +8,6 @@ class ChatbotGUI:
     def __init__(self):
         # OpenAI API configuration
         openai.api_key = key.OPENAI_KEY
-
     def jsonToArray(self, jsonFile):
         with open(jsonFile, "r", encoding="utf-16") as file:
                 logs_data = json.load(file)
@@ -20,7 +19,7 @@ class ChatbotGUI:
             df.loc[len(df)] = new_row
         return df
 
-    def start_query_session(self, dataFrame):
+    def start_query_session(self, user_input, dataFrame):
         messag=[{"role": "system", "content": "You are a chatbot."}]
         
         ## build a chat history: you can CONDITION the bot on the style of replies you want to see - also getting weird behaviors... such as KanyeGPT
@@ -33,30 +32,28 @@ class ChatbotGUI:
         for user_message, bot_message in zip(history_user, history_bot):
             messag.append({"role": "user", "content": str(user_message)})
             messag.append({"role": "system", "content": str(bot_message)})
-        
-        #store the chat and update it each time
-        print("Hello, I am your Personal Virtual Technician, ask me questions about your system! Enter 'exit' if you'd like to stop chatting with me.")
-        while True:
-            user_input = input("Your input: ")
-            messag.append({"role": "user", "content": str(user_input)})
-            response = openai.ChatCompletion.create(
-             model="gpt-3.5-turbo",
-             messages=messag
-            )
-            result = ''
-            for choice in response.choices:
-                result += choice.message.content
-            history_bot.append(result)
-            if user_input.lower() == 'exit':
-                print("Exiting the chat!")
-                break
-            else:
-                print("AI Response:")
-                print(result)
+        messag.append({"role": "user", "content": str(user_input)})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messag
+        )
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content
+        history_bot.append(result)
+        return result
 
 def main():
     chatbot_gui = ChatbotGUI()
-    df = chatbot_gui.jsonToArray("system_logs_last_30_days.json")
-    chatbot_gui.start_query_session(df)
+    df = chatbot_gui.jsonToArray("dataBases/system_logs_last_30_days.json")
+    print("Hello, I am your Personal Virtual Technician, ask me some questions about your system and I will try my best to answer!")
+    while True:
+        user_input = input("Input your prompt here: ")
+        if user_input == 'exit':
+            break
+        else:
+            print("AI message:")
+            print(chatbot_gui.start_query_session(user_input ,df))
+        
 if __name__ == "__main__":
     main()
