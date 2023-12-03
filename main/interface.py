883ce3cf10eca5f8fd6cfc99ff8fd044fac12c60
nikhen-s <nikhen.sanjaya2003@gmail.com>
2023-12-03 01:06:43 +0100

    Prompted OpenAI for structured answers for user questions on System

--- main/interface.py
+++ main/interface.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 from tkinter import ttk, messagebox, END
 import json
 import pandas as pd
@@ -6,47 +5,9 @@ import requests
 import key
 import openai
 class ChatbotGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("Chatbot Interface")
-
-        # Style configuration for themed widgets
-        style = ttk.Style()
-        style.theme_use('clam')  # You can experiment with different themes
-
-        # Entry widget for user input
-        self.user_input = ttk.Entry(master, width=40)
-        self.user_input.pack(padx=10, pady=10)
-
-        # Button to show all logs
-        self.show_logs_button = ttk.Button(master, text="Send Message", command=self.send_message)
-        self.show_logs_button.pack(pady=10)
-
-        # Text widget to display conversation history
-        self.conversation_history = tk.Text(master, wrap="word", width=60, height=20)
-        self.conversation_history.pack(padx=10, pady=10)
-
-        # Initialize conversation history
-        self.conversation_history.insert(tk.END, "Chatbot: Hello! How can I help you?\n")
-
+    def __init__(self):
         # OpenAI API configuration
         openai.api_key = key.OPENAI_KEY
-
-    def send_message(self):
-        # Get user input
-        user_message = self.user_input.get()
-
-        # Display user's message in the conversation history
-        self.conversation_history.insert(tk.END, f"User: {user_message}\n")
-
-        # Call OpenAI API
-        response = self.query_openai({"text": user_message})
-
-        # Print the response for debugging
-        print("OpenAI Response:", response)
-
-        # Display OpenAI's response in the conversation history
-        self.conversation_history.insert(tk.END, f"Chatbot: {response}\n")  # Adjust this line accordingly
 
     def jsonToArray(self, jsonFile):
         with open(jsonFile, "r", encoding="utf-16") as file:
@@ -60,13 +21,14 @@ class ChatbotGUI:
         return df
 
     def query_response(self, prompt, dataFrame):
-        messag=[{"role": "system", "content": "I am a user trying to understand my system and you are a computer systems professional who can help me explain my questions I have about it. For each question, you will (a) give a short concise 1 sentence answer to user's question (2) show proof for the answer directly copied from the (Message) column from database sent."}]
+        messag=[{"role": "system", "content": "You are a chatbot."}]
         
         ## build a chat history: you can CONDITION the bot on the style of replies you want to see - also getting weird behaviors... such as KanyeGPT
-        history_bot = ["Yes, I'm ready! Please provide to me your first question about your system"]
-    
+        history_bot = ["I'm ready! Ask me any question about your system"]
         # ask ChatGPT to return STRUCTURED, parsable answers that you can extract easily - often better providing examples of desired behavior (1-2 example often enough)
-        history_user = ["This is the database:" + dataFrame]
+        history_user = ["I am a user trying to understand my system and you are a computer systems professional who can help me explain my questions I have about it. For each answer you give, you need to show proof for the answer directly copied from the (Message) column in the database. For example, if someone asks, what installations have happened in the system? You can reply with, there have been _ number of installations that have happened in the system. Examples of things you have installed include: 1. An antivirus shown in from the message: Installation réussie : Windows a installé la mise à jour suivante : Mise à jour de la sélection disjointe pour Microsoft Defender Antivirus – 2267602 Ko (version 1.401.1546.0) – Canal actuel (large). Please use the dataset given here:"]
+        history_user.append("This is the database you should analyse from: " + dataFrame)
+        history_bot.append("Okay! I will use the database you have given me and give you my best answer!")
 
         for user_message, bot_message in zip(history_user, history_bot):
             messag.append({"role": "user", "content": str(user_message)})
@@ -85,12 +47,17 @@ class ChatbotGUI:
         return result
 
 def main():
-    root = tk.Tk()
-    chatbot_gui = ChatbotGUI(root)
-    #root.mainloop()
-    df = chatbot_gui.jsonToArray("system_logs_last_7_days.json")
-    print("CHATGPT RESPONSE:\n")
-    print(chatbot_gui.query_response("How is my network connection behaving?",df))
-
+    chatbot_gui = ChatbotGUI()
+    df = chatbot_gui.jsonToArray("system_logs_last_30_days.json")
+    print("Hello, I am your Personal Virtual Technician, ask me questions about your system!")
+    while True:
+        user_input = input("Your input: ")
+        if user_input.lower() == 'exit':
+            print("Exiting the chat!")
+            break
+        else:
+            print("CHATGPT's RESPONSE:")
+            print(chatbot_gui.query_response(user_input,df))
+    
 if __name__ == "__main__":
     main()
